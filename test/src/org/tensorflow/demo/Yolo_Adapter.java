c79979d55b38618f3826a0306fc8a4dc466c5b97
soosoo030 <whereru511@gmail.com>
2022-09-25 20:01:39 +0900

    [물체인식] 단어 선택하면 dialog로 뜨게 하기

--- test/src/org/tensorflow/demo/Yolo_Adapter.java
+++ test/src/org/tensorflow/demo/Yolo_Adapter.java
@@ -1,13 +1,30 @@
 package org.tensorflow.demo;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.JsonElement;
+
+import java.util.HashMap;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Yolo_Adapter extends RecyclerView.Adapter<Yolo_Adapter.ItemViewHolder> {
     private LayoutInflater inflater;
@@ -41,11 +58,39 @@ public class Yolo_Adapter extends RecyclerView.Adapter<Yolo_Adapter.ItemViewHold
     public class ItemViewHolder extends RecyclerView.ViewHolder{
         public final View mView;
         private TextView Yolo_title;
+        private Button Yolo_selectBtn;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             mView=itemView;
             Yolo_title=itemView.findViewById(R.id.Yolo_title);
+            Yolo_selectBtn = itemView.findViewById(R.id.Yolo_selectBtn);
+            Yolo_selectBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Log.e("음",String.valueOf(Yolo_title.getText()));
+                    custom_dialog(view, getAdapterPosition());
+                }
+            });
         }
     }
+    public void custom_dialog(View v, int position) {
+        View dialogView = inflater.inflate(R.layout.dialog_yoloselect, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setView(dialogView);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        TextView out_btn = dialogView.findViewById(R.id.out_btn);
+        out_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                alertDialog.dismiss();
+            }
+        });
+    }
+
 }
